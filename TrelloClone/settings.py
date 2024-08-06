2ea5fbcae30f656dac0b9a1ed8f093304b38b7dd
vladikD <93707032+vladikD@users.noreply.github.com>
2024-08-07 00:14:09 +0300

    added cors

--- TrelloClone/settings.py
+++ TrelloClone/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'dj_rest_auth',
     'users',
     'social_django',
+    'corsheaders',
 
 ]
 
@@ -67,6 +68,14 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     "allauth.account.middleware.AccountMiddleware",
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
+]
+
+CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
 ]
 
 ROOT_URLCONF = 'TrelloClone.urls'
